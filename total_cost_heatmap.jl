b5ddb342ef8e3069cc7b9c825ae7aad26303f9cc
jacobusmmsmit <jacobusmmsmit@gmail.com>
2021-08-04 18:27:20 +0100

    Add homo-egualis path plot

--- total_cost_heatmap.jl
+++ total_cost_heatmap.jl
@@ -42,13 +42,16 @@ while difference > 1e-6
 end
 
 plot!(heatcost, [p1], [q1], st = :scatter, ms = scattersize, label = "Minimum Total Cost")
+plot!(colorbar_title = "Total Cost")
+plot!(size = (500, 300))
 
+heatcost_extrapoints
 begin
     possibleroots = first(find_valid_roots(q -> ForwardDiff.derivative(ψ -> total_cost(pstar(ψ, parameters...), ψ, parameters...), q)))
     roots = possibleroots[ForwardDiff.derivative.(q -> ForwardDiff.derivative(ψ -> total_cost(pstar(ψ, parameters...), ψ, parameters...), q), possibleroots) .> 0]
     q_p1br = roots[findmin(abs.(roots .- q))[2]]
     p_p1br = pstar(q_p1br, parameters...)
-    plot!(heatcost, [p_p1br], [q_p1br], st = :scatter, ms = scattersize, label = "Min where P1 best responds")
+    heatcost_extrapoints = plot(heatcost, [p_p1br], [q_p1br], st = :scatter, ms = scattersize, label = "Min where P1 best responds")
 end
 
 begin
@@ -56,8 +59,42 @@ begin
     roots = possibleroots[ForwardDiff.derivative.(p -> ForwardDiff.derivative(ψ -> total_cost(ψ, qstar(ψ, parameters...), parameters...), p), possibleroots) .> 0]
     p_p2br = roots[findmin(abs.(roots .- p))[2]]
     q_p2br = qstar(p_p2br, parameters...)
-    plot!(heatcost, [p_p2br], [q_p2br], st = :scatter, ms = scattersize, label = "Min where P2 best responds")
+    plot!(heatcost_extrapoints, [p_p2br], [q_p2br], st = :scatter, ms = scattersize, label = "Min where P2 best responds")
 end
-plot!(colorbar_title = "Total Cost")
-plot!(size = (500, 300))
-savefig("outputs/total_cost_contour.pdf")
+
+
+
+# savefig("outputs/total_cost_contour.pdf")
+
+probability_path = he_equilibrium_probability.(λ1, λ2, α, μ, ν, 0:0.001:1)
+
+begin
+    heatcost_path = plot(heatcost, legend = :topright, size = (500, 300), xlims = (0.41, 0.44), ylims = (0.59, 0.615))
+    n = 5
+    for i in 1:n
+        interval = Int(floor(length(probability_path)/n))
+        slice = 1 + (i-1) * interval: i*interval
+        plot!(heatcost_path,
+            first.(probability_path[slice]),
+            last.(probability_path[slice]),
+            lc = :black,
+            lw = 1,
+            ls = :solid,
+            arrow = i == n ? false : true,
+            label = i == 1 ? "Homo-Egualis path" : "")
+        # if i != n
+        #     annotate!(
+        #         heatcost_path,
+        #         probability_path[i*interval + 1]...,
+        #         text(
+        #             round(collect(0:0.001:1)[i*interval], digits = 2),
+        #             :bottom,
+        #             :green,
+        #             11)
+        #         )
+        # end
+    end
+    heatcost_path
+end
+
+savefig("outputs/homo-egualis_path_contour.pdf")
